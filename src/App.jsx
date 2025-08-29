4457fbc1d8cd509d5cb893f8f1f43128f17f9060
itslokeshx <lokeshvijayraina@gmail.com>
2025-08-29 23:03:00 +0530

    shuffling option addded

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
-  const [shuffle, setShuffle] = useState(0);
+  let [ shuffledPokemon, setShuffledPokemon]=useState([])
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=24&offset=${shuffle}`)
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=12&offset=0")
       .then((res) => res.json())
       .then((data) => {
 
@@ -24,22 +24,35 @@ export default function App() {
           setPokemon(results);
         });
       });
-  }, [shuffle]);
-function Increment(){
-  setShuffle(shuffle=> shuffle+1)
-}
+  }, [pokemon]);
+
+  function shufflePokemon(){
+     let x =pokemon
+     for( let i=x.length -1; i>0;i--)
+     {
+        let random = Math.floor(Math.random() * (i+1));
+
+        [x[i],x[random]] = [x[random], x[i]]
+         
+     }
+       setShuffledPokemon( shuffledPokemon= pokemon)
+    
+   
+  }
+
+  
 
   return (
     <div>
         <div className="pokemonsGrid">
-      {pokemon.map((p) => (
+      {shuffledPokemon.map((p) => (
         <div className="pokemons" key={p.name}>
           <img src={p.image} alt={p.name} />
           <p>{p.name}</p>
         </div>
       ))}
 </div>
-      <button onClick={Increment} >change! {shuffle}</button>
+      <button onClick={shufflePokemon} >Shuffle!</button>
     </div>
   );
 }
