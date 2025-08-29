9a90aa868605984ce46026d64f915421461c9357
itslokeshx <lokeshvijayraina@gmail.com>
2025-08-29 23:19:59 +0530

    organized code with styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,48 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
-  let [ shuffledPokemon, setShuffledPokemon]=useState([])
+  const [shuffledPokemon, setShuffledPokemon] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=12&offset=0")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=12&offset=24")
       .then((res) => res.json())
       .then((data) => {
-
         const promises = data.results.map((p) =>
           fetch(p.url)
             .then((res) => res.json())
-            .then((details) => {
-              return {
-                name: details.name,
-                image: details.sprites.front_default,
-              };
-            })
+            .then((details) => ({
+              name: details.name,
+              image: details.sprites.front_default,
+            }))
         );
-
         Promise.all(promises).then((results) => {
           setPokemon(results);
+          setShuffledPokemon(results);
         });
       });
-  }, [pokemon]);
+  }, []);
 
-  function shufflePokemon(){
-     let x =pokemon
-     for( let i=x.length -1; i>0;i--)
-     {
-        let random = Math.floor(Math.random() * (i+1));
-
-        [x[i],x[random]] = [x[random], x[i]]
-         
-     }
-       setShuffledPokemon( shuffledPokemon= pokemon)
-    
-   
+  function shufflePokemon() {
+    let x = [...pokemon];
+    for (let i = x.length - 1; i > 0; i--) {
+      let random = Math.floor(Math.random() * (i + 1));
+      [x[i], x[random]] = [x[random], x[i]];
+    }
+    setShuffledPokemon(x);
   }
 
-  
-
   return (
-    <div>
-        <div className="pokemonsGrid">
-      {shuffledPokemon.map((p) => (
-        <div className="pokemons" key={p.name}>
-          <img src={p.image} alt={p.name} />
-          <p>{p.name}</p>
-        </div>
-      ))}
-</div>
-      <button onClick={shufflePokemon} >Shuffle!</button>
+    <div className="app">
+      <h1 className="title">Pokémon Shuffle</h1>
+      <div className="pokemonsGrid">
+        {shuffledPokemon.map((p) => (
+          <div className="pokemons" key={p.name}>
+            <img src={p.image} alt={p.name} />
+            <p>{p.name}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={shufflePokemon}>Shuffle!</button>
     </div>
   );
 }
-
-
